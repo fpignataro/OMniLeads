1cb2844aa866606573037924deca71c7d80acf6f
Isachi <isachiqba@gmail.com>
2023-03-27 19:55:22 -0500

    Poder almacenar los static files de django en Object Storage

--- ominicontacto/settings/defaults.py
+++ ominicontacto/settings/defaults.py
@@ -191,8 +191,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_STORAGE = (os.getenv("STATICFILES_STORAGE") or
+                       "django.contrib.staticfiles.storage.StaticFilesStorage")
 
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
