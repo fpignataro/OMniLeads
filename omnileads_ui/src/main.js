7075e7cf27dc1613ffa606784801c233fbdf8dd7
Emiliano González <emighinoomldev@gmail.com>
2023-10-30 13:36:45 +0000

    Mejoras:
    - Creacion de vista y componentes de Front
    - Se crean servicios, store y urls en Front
    - Se modifiga vue.config para agregar vista
    - Se agrega nuevo evento para modal de Django Back/Front
    - Se modifica serializador de conversacion en Back
    - Fix en eventos de create/update de contacto entre diferentes iframes

--- omnileads_ui/src/main.js
+++ omnileads_ui/src/main.js
@@ -56,6 +56,7 @@ import SplitButton from 'primevue/splitbutton';
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
 import Panel from 'primevue/panel';
+import SpeedDial from 'primevue/speeddial';
 
 // Idiomas
 import { createI18n } from 'vue-i18n';
@@ -113,6 +114,7 @@ app.component('SplitButton', SplitButton);
 app.component('TabView', TabView);
 app.component('TabPanel', TabPanel);
 app.component('Panel', Panel);
+app.component('SpeedDial', SpeedDial);
 
 // Register Helpers
 app.provide('$helpers', Helpers);
